88a3ff30ca511e2346e0886e8133138c7ea4ee60
Vinicebo <viniciusdaluzlcp@gmail.com>
2024-10-20 16:25:54 -0300

    ajeitando o banner

--- src/pages/Detalhamento.jsx
+++ src/pages/Detalhamento.jsx
@@ -2,14 +2,16 @@ import { useParams } from "react-router-dom";
 import Base from "./Base";
 import Banner from "../components/Banner/Banner.jsx"; 
 import dados_banner from "../data/dados-banner.json"; 
+import Header from "../components/Header/Header.jsx";
 
-const Detalhamento = () => {
+const Detalhamento = (props) => {
   const { id } = useParams(); 
   const banner = dados_banner.find(el => el.id === id); 
 
   return (
-    <Base>
-      {banner && ( 
+    <>
+    <Header></Header>
+    {banner && ( 
         <Banner 
           key={banner.id}
           id={banner.id}
@@ -17,7 +19,8 @@ const Detalhamento = () => {
           subtitulo={banner.subtitulo}
         />
       )}
-    </Base>
+    
+    </>
   );
 };
 
