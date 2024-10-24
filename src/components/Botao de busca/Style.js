1ac7d28cb0826162e00a98230c5d780a44cf36ef
GabriellCorrea <gabrielcs.1003@gmail.com>
2024-10-24 16:11:39 -0300

    Responsividade botao de busca

--- src/components/Botao de busca/Style.js
+++ src/components/Botao de busca/Style.js
@@ -1,21 +1,29 @@
 import styled from "styled-components";
 
-
 const Botao_Container = styled.div`
-  display: flex;        
+  position: relative;
+  display: flex;
   align-items: center;
   justify-content: center;
   border: 2px solid #002855;
   border-radius: 25px;
-  padding: 10px 20px; 
+  padding: 10px 20px;
   background-color: white;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
-  width: 400px; 
-  position: absolute;
-  top: 55vh; 
+  width: 400px;
+  top: 9em; 
   left: 50%;
-  transform: translate(-50%, 50%);
+  transform: translate(-50%, -50%); 
   font-family: 'Roboto Condensed', sans-serif;
+
+  @media (max-width: 768px) {
+    width: 300px;
+  }
+
+  @media (max-width: 385px) {
+    width: 250px;
+    padding: 8px 15px;
+  }
 `;
 
 const Botao_Lupa = styled.img`
@@ -23,22 +31,34 @@ const Botao_Lupa = styled.img`
   width: 20px;
   margin-right: 350px;
 
+  @media (max-width: 768px) {
+    margin-right: 250px;
+  }
+
+  @media (max-width: 385px) {
+    margin-right: 200px;
+  }
 `;
 
 const Botao_busca = styled.span`
   font-size: 18px;
   color: #002855;
   margin-right: 10px;
-  position : absolute;
+  position: absolute;
+
+  @media (max-width: 385px) {
+    font-size: 16px;
+  }
 `;
 
 const Botao_Filtro = styled.img`
   height: 20px;
   width: 20px;
   margin-left: 10px;
-`
 
+  @media (max-width: 385px) {
+    margin-left: 5px;
+  }
+`;
 
-
-
-export {Botao_Container, Botao_busca, Botao_Filtro, Botao_Lupa}
+export { Botao_Container, Botao_busca, Botao_Filtro, Botao_Lupa };
